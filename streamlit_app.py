cebdf2c33f67db0bda7caf997cc65d0203e83bbc
srinn-snowflake <115489726+srinn-snowflake@users.noreply.github.com>
2022-10-10 14:50:18 -0500

    Update streamlit_app.py
    Added Breakfast menutitle
    Added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
+BREAKFAST MENU
+
+Omega 3 & Blueberry Oatmeal
+
+Kale, Spinich and Rocket Smoothie
+
+Hard Boiled Free_Range Eggs
+
