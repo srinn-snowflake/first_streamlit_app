eac729fff80ae71c4d1ddb7ecad88a7dd27276e3
srinn-snowflake <115489726+srinn-snowflake@users.noreply.github.com>
2022-10-10 15:17:43 -0500

    Update streamlit_app.py
    Add emoji's to menau

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('BREAKFAST MENU')
 
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinich and Rocket Smoothie')
+streamlit.text('🥬 Kale, Spinich and Rocket Smoothie')
 
-streamlit.text('Hard Boiled Free_Range Eggs')
+streamlit.text('🐔 Hard Boiled Free_Range Eggs')
 
