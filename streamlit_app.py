11f5e3030b6b30eb7fbb1104ccecd8ae6a7447bb
srinn-snowflake <115489726+srinn-snowflake@users.noreply.github.com>
2022-10-11 16:23:57 -0500

    Update streamlit_app.py
    corrected syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list),['Avocado', 'Strawberries')
+streamlit.dataframe(my_fruit_list),['Avocado', 'Strawberries']
 
  
